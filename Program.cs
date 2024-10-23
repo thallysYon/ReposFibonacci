c7c3702a17b0ddc9d6fa406bca4a126949912ac3
ville01 <ville01@LAPTOP-S9I3AA7A>
2024-10-23 15:34:36 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int numero;
+int anterior = 0;
+int atual = 1;
+bool pertence;
+
+Console.WriteLine("Informe um número para verificar se pertence à sequência de Fibonacci:");
+numero = int.Parse(Console.ReadLine());
+
+pertence = (numero == 0 || numero == 1) ? true : false;
+
+while (atual <= numero && !pertence)
+{
+    pertence = (atual == numero) ? true : false;
+
+    int proximo = anterior + atual;
+
+    anterior = atual;
+    atual = proximo;
+}
+
+Console.WriteLine(pertence
+    ? $"O número {numero} pertence à sequência de Fibonacci."
+    : $"O número {numero} NÃO pertence à sequência de Fibonacci.");
